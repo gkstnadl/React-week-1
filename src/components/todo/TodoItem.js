c1bf704c9655de9b1d1e97aea353e634269febca
gkstnadl <gkstnadl12@naver.com>
2024-01-23 13:45:49 +0900

    Working부분과 Done부분 제목, 내용 클래스네임 동일화

--- src/components/todo/TodoItem.js
+++ src/components/todo/TodoItem.js
@@ -3,8 +3,8 @@ import React from 'react';
 // Todo 및 Done 항목 렌더링을 위한 컴포넌트
 const TodoItem = ({ todo, onDeleteTodo, onToggleTodoStatus }) => (
     <div className='content'>
-        <h2 className={todo.isDone ? '' : 'input-title'}>{todo.title}</h2>
-        <p className={todo.isDone ? '' : 'input-main-text'}>{todo.content}</p>
+        <h2 className='input-title'>{todo.title}</h2>
+        <p className='input-main-text'>{todo.content}</p>
         <div className='buttons'>
             <button className='delete-btn' onClick={() => onDeleteTodo(todo.id)}>삭제하기</button>
             <button className={todo.isDone ? 'cancel-btn' : 'done-btn'} onClick={() => onToggleTodoStatus(todo.id)}>
